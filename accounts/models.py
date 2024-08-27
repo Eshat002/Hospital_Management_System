b6c851a16ddde7390ef754bb7304e229bafc3441
eshat002 <eshatjubayer22@outlook.com>
2024-08-28 00:00:18 +0600

    Add accounts app create account using signals

---
+++ accounts/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+User = get_user_model()
+
+
+class Account(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=80)
+    last_name = models.CharField(max_length=80)
+
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return f"Account - @{self.user.username}"
+
+
+@receiver(post_save, sender=User)
+def create_user_account(sender, instance, created, **kwargs):
+    if created:
+        Account.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_account(sender, instance, **kwargs):
+    if hasattr(instance, "account"):
+        instance.account.first_name = instance.first_name
+        instance.account.last_name = instance.last_name
+        instance.account.save()
